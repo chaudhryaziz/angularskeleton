01637ce61aa26d923265dcb557b0a08005fef32f
chaudhryaziz <chaudhryaziz@gmail.com>
2016-04-08 16:32:58 -0400

    Added files via upload
    typo in source.

--- insert.php
+++ insert.php
@@ -5,7 +5,7 @@ $fname = mysql_real_escape_string($data->fname);
 $lname = mysql_real_escape_string($data->lname);
 $dept = mysql_real_escape_string($data->dept);
 mysql_connect("localhost","root","PASSWORD");
-mysql_select_db("company");
+mysql_select_db("companyDB");
 mysql_query("INSERT INTO employee(`emp_no`, `first_name`, `last_name`, `dept_name`)VALUES('".$empno."','".$fname."','".$lname."','".$dept."')");
 
 
